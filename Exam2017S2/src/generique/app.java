e2226fc0bfbd3d0029782cbb635a8bf70eb55d48
Ghassen <38542532+ghassenbelkhir@users.noreply.github.com>
2018-12-19 01:11:33 +0100

    Correct question 2  exam 2018

--- Exam2017S2/src/generique/app.java
+++ Exam2017S2/src/generique/app.java
@@ -15,11 +15,11 @@ import java.util.function.Supplier;
 public class app {
 
 
-	// Examen Deuxième Session  28/06/2017
+	// Examen DeuxiÃ¨me Session  28/06/2017
 
-	//une méthode qui prend en argument deux listes l1, l2 d'objets Comparable entre eux et
-	//qui remplace chaque élément l1 par l'élément à la même position en l2, si ce dernier est
-	//plus grand (on suppose que les deux listes aient la même taille) ;
+	//une mÃ©thode qui prend en argument deux listes l1, l2 d'objets Comparable entre eux et
+	//qui remplace chaque Ã©lÃ©ment l1 par l'Ã©lÃ©ment Ã  la mÃªme position en l2, si ce dernier est
+	//plus grand (on suppose que les deux listes aient la mÃªme taille) ;
 
 	public <T extends Comparable<T> > void question1(List<? super T > listdst ,List<? extends T> listsrc)
 	{
@@ -34,8 +34,8 @@ public class app {
 
 	
 
-	//une méthode qui prend en argument une liste l de Supplier et qui renvoie la liste qui
-	//contient, pour chaque element s de l, les trois premiers éléments produits par s ;
+	//une mÃ©thode qui prend en argument une liste l de Supplier et qui renvoie la liste qui
+	//contient, pour chaque element s de l, les trois premiers Ã©lÃ©ments produits par s ;
 
 	public <T> List<T> question2(List<Supplier<? extends T>> l)
 	{
@@ -51,9 +51,9 @@ public class app {
 
 	}
 
-	//une méthode qui prend en argument une liste de listes l, et un élément e. On dit qu'un liste
-	//est "bonne" si elle contient e. La méthode doit renvoyer l'ensemble de tous les éléments
-	//qui appartiennent à au moins une bonne liste de l.
+	//une mÃ©thode qui prend en argument une liste de listes l, et un Ã©lÃ©ment e. On dit qu'un liste
+	//est "bonne" si elle contient e. La mÃ©thode doit renvoyer l'ensemble de tous les Ã©lÃ©ments
+	//qui appartiennent Ã  au moins une bonne liste de l.
 
 	public <T> List<T> question3(List<? extends List<? extends T>> l,  T e)
 	{
@@ -69,10 +69,10 @@ public class app {
 	}
 
 
-	//Examen Première Session   14/12/2016
+	//Examen PremiÃ¨re Session   14/12/2016
 
-	// une méthode qui prend en entrée deux Supplier d'objets Comparable et qui renvoie le
-	//plus petits élément produit par les deux suppliers ;
+	// une mÃ©thode qui prend en entrÃ©e deux Supplier d'objets Comparable et qui renvoie le
+	//plus petits Ã©lÃ©ment produit par les deux suppliers ;
 
 	public <T  extends Comparable<T>> T MinSupplier(Supplier<? extends T> sup1,Supplier<? extends T> sup2)
 	{
@@ -84,8 +84,8 @@ public class app {
 			return sup1.get();
 	}
 
-	//une méthode qui prend en entrée une List ls et un Comparator et qui renvoie une List
-	//qui contient les trois plus petits éléments de ls ;
+	//une mÃ©thode qui prend en entrÃ©e une List ls et un Comparator et qui renvoie une List
+	//qui contient les trois plus petits Ã©lÃ©ments de ls ;
 
 	public static  <T extends Comparable<T>> List<T> petitElm(List<? extends T> ls,Comparator<T> cmp)
 	{
@@ -99,8 +99,8 @@ public class app {
 	}
 
 
-	//une méthode qui prend en entrée une Collection c de List et qui renvoie une List qui
-	//contient tous les éléments qui apparaissent dans toutes les listes de c ;
+	//une mÃ©thode qui prend en entrÃ©e une Collection c de List et qui renvoie une List qui
+	//contient tous les Ã©lÃ©ments qui apparaissent dans toutes les listes de c ;
 
 	public static <T> List<T> includeLists(Collection<List<? extends T>> c)
 	{
@@ -117,11 +117,11 @@ public class app {
 
 
 
-	//Examen Première Session   20/12/2017
+	//Examen PremiÃ¨re Session   20/12/2017
 
-	// une methode qui prend en entréé deux List et un comarator et qui renvoie une list qui contient les elements
-	// des deux listes triés en ordre croissant
-	public static <T > List<T> ListTrie(List<? extends T> l1,List< ?  extends T> l2, Comparator<T> cmp)
+	// une methode qui prend en entrÃ©Ã© deux List et un comarator et qui renvoie une list qui contient les elements
+	// des deux listes triÃ©s en ordre croissant
+	public static <T extends Comparable<T> > List<T> ListTrie(List<? extends T> l1,List< ?  extends T> l2, Comparator<T> cmp)
 	{
 		List<T> ll = new ArrayList<>();
 
@@ -136,22 +136,22 @@ public class app {
 	}
 
 
-	//une méthode qui prend en entrée une list l de Map, et un object k, et qui renvoie une List qui contient tous les élements
-	// qui sont associés à k par une des maps de la list l.
+	//une mÃ©thode qui prend en entrÃ©e une list l de Map, et un object k, et qui renvoie une List qui contient tous les Ã©lements
+	// qui sont associÃ©s Ã  k par une des maps de la list l.
 
-	public static <K,T> List<T> AssocieMap(List<Map<? extends K,? extends T>> listOfMap, K k)
+	public static <K,T> List<T> AssocieMap(List<? extends Map< K,T>> listOfMap, K k)
 	{
 		List<T> ll = new ArrayList<>();
-		for(Map<? extends K, ? extends T> mymap :listOfMap )
+		for(Map<K,T> mymap :listOfMap )
 		{
-			ll.add(mymap.get(k));
+			if(mymap.containsKey(k)) ll.add(mymap.get(k));
 		}
 		return ll;
 	}
 
 
 
-	// une méthode qui prend en entrée un BiPredicate p d'objets Comparable et qui renvoie le BiPredicate qui test 
+	// une mÃ©thode qui prend en entrÃ©e un BiPredicate p d'objets Comparable et qui renvoie le BiPredicate qui test 
 	// si p est vrai et si le premier argument est plus petit que le deuxieme
 
 	public static <T extends Comparable<T>>  BiPredicate<T,T> TestPredicate(BiPredicate<T,T> p)
